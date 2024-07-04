8eb1dcdbb705e81c5209456fdb90b2218649fa40
Saugat Tamang <itsurenkanakueta@gmail.com>
2024-07-04 10:57:01 +0000

    First Test Case

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Cover2.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "A person working on a laptop";
 
 /**
  * Project list
@@ -31,28 +31,26 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Get Closest",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A mobile game that utilizes mathematical expression to reach a goal built using Kivy Library, python",
+    url: "https://github.com/ITSURENXD/Get_Closest",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Computer Graphics for Uni",
+    description: "A display of various graphics applications for university applications.",
+    url: "https://github.com/ITSURENXD/Graphics",
   },
   {
-    title: "My Resume Site",
+    title: "Data Structures and Algorithms for Uni",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A display of various data structures and algorithm implementations for university applications",
+    url: "https://github.com/ITSURENXD/DSA",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "My Portfolio Site",
+    description: "Created using netlify on github repository",
+    url: "https://github.com/ITSURENXD/Portfolio",
   },
 ];
 

4cba0deb1592d10f5cc2a736280dcc5ccc8290af
Saugat Tamang <itsurenkanakueta@gmail.com>
2024-07-04 11:57:09 +0000

    Trying to fix the image issue

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/ProfileLayer1.jpg";
+import image from "../images/ProfileLayer1.jpeg";
 
 const imageAltText = "A cartoonish Avatar of Itsuren";
 
